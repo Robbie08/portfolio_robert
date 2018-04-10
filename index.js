65ccc6c8685b42ca9f4c9a0aaea9dd5d738060f7
ortiz <Roberinho08@gmail.com>
2018-04-10 10:58:57 -0700

    Backend files updated

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
 const app = express(); //creates an express application
+
+//set port
+const port = process.env.PORT || 8080
+
+
 var path = require('path');
 
 app.use(express.static(__dirname));
 
 
 //handles the route 
-app.get('/',(req,res) => {
+app.get("/", function(req,res) {
     console.log(__dirname);
-    
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.render("index")
+});
+
+
+app.listen(function () {
+    console.log("App running ")
 });
 
 app.listen(port); 
